d73cff96d451e528ea91aaed7924e3dc04f7bbc8
fanhualei <fanhl@189.cn>
2019-07-17 23:56:28 +0800

    模板

---
+++ example-cpp/src/21-templates/templates-function.cpp
@@ -0,0 +1,43 @@
+/**
+ * C++ 模板
+ *  模板是泛型编程的基础，泛型编程即以一种独立于任何特定类型的方式编写代码。
+ *  模板是创建泛型类或函数的蓝图或公式。
+ *  库容器，比如迭代器和算法，都是泛型编程的例子，它们都使用了模板的概念。
+ *  每个容器都有一个单一的定义，比如 向量，
+ *  我们可以定义许多不同类型的向量，比如 vector <int> 或 vector <string>。
+ *  您可以使用模板来定义函数和类，接下来让我们一起来看看如何使用。
+ * 
+ * 函数模板
+ *  template <class type> ret-type func-name(parameter list)
+ * 
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+template <typename T>
+inline T const& Max (T const& a, T const& b) 
+{ 
+    return a < b ? b:a; 
+} 
+int main ()
+{
+ 
+    int i = 39;
+    int j = 20;
+    cout << "Max(i, j): " << Max(i, j) << endl; 
+ 
+    double f1 = 13.5; 
+    double f2 = 20.7; 
+    cout << "Max(f1, f2): " << Max(f1, f2) << endl; 
+ 
+    string s1 = "Hello"; 
+    string s2 = "World"; 
+    cout << "Max(s1, s2): " << Max(s1, s2) << endl; 
+ 
+   return 0;
+}
