d5ee0878f6d9ecdb52004126c5279c41ff458e26
fanhualei <fanhl@189.cn>
2019-07-17 22:05:47 +0800

    add overloading codes

---
+++ example-cpp/src/13-overloading/relational-operators-overloading.cpp
@@ -0,0 +1,65 @@
+/**
+ * C++ 关系运算符重载
+ *  C++ 语言支持各种关系运算符（ < 、 > 、 <= 、 >= 、 == 等等），它们可用于比较 C++ 内置的数据类型。
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+ 
+class Distance
+{
+   private:
+      int feet;             // 0 到无穷
+      int inches;           // 0 到 12
+   public:
+      // 所需的构造函数
+      Distance(){
+         feet = 0;
+         inches = 0;
+      }
+      Distance(int f, int i){
+         feet = f;
+         inches = i;
+      }
+      // 显示距离的方法
+      void displayDistance()
+      {
+         cout << "F: " << feet << " I:" << inches <<endl;
+      }
+      // 重载负运算符（ - ）
+      Distance operator- ()  
+      {
+         feet = -feet;
+         inches = -inches;
+         return Distance(feet, inches);
+      }
+      // 重载小于运算符（ < ）
+      bool operator <(const Distance& d)
+      {
+         if(feet < d.feet)
+         {
+            return true;
+         }
+         if(feet == d.feet && inches < d.inches)
+         {
+            return true;
+         }
+         return false;
+      }
+};
+int main()
+{
+   Distance D1(11, 10), D2(5, 11);
+ 
+   if( D1 < D2 )
+   {
+      cout << "D1 is less than D2 " << endl;
+   }
+   else
+   {
+      cout << "D2 is less than D1 " << endl;
+   }
+   return 0;
+}
---
+++ example-cpp/src/13-overloading/unary-operators-overloading.cpp
@@ -0,0 +1,54 @@
+/**
+ * C++ 一元运算符重载
+ * https://www.runoob.com/cplusplus/unary-operators-overloading.html
+ *  递增运算符（ ++ ）和递减运算符（ -- ）
+ *  一元减运算符，即负号（ - ）
+ *  逻辑非运算符（ ! ）
+ * 
+ */
+
+
+
+#include <iostream>
+using namespace std;
+ 
+class Distance
+{
+   private:
+      int feet;             // 0 到无穷
+      int inches;           // 0 到 12
+   public:
+      // 所需的构造函数
+      Distance(){
+         feet = 0;
+         inches = 0;
+      }
+      Distance(int f, int i){
+         feet = f;
+         inches = i;
+      }
+      // 显示距离的方法
+      void displayDistance()
+      {
+         cout << "F: " << feet << " I:" << inches <<endl;
+      }
+      // 重载负运算符（ - ）
+      Distance operator- ()  
+      {
+         feet = -feet;
+         inches = -inches;
+         return Distance(feet, inches);
+      }
+};
+int main()
+{
+   Distance D1(11, 10), D2(-5, 11);
+ 
+   -D1;                     // 取相反数
+   D1.displayDistance();    // 距离 D1
+ 
+   -D2;                     // 取相反数
+   D2.displayDistance();    // 距离 D2
+ 
+   return 0;
+}
